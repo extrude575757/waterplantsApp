74d0fd8277a7c5c8cb053b28f2bcdc0a3c7abbb6
extrude575757 <nicholas.a.lacapria@gmail.com>
2020-10-24 06:20:45 -0700

    New styles for https://pedantic-lichterman-c811ec.netlify.app/

---
+++ build/precache-manifest.e2fab12c33764129e785e4af605c6e2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ff9644ac0e335dca8627ab6af1766ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90517af4bef2d322917e",
+    "url": "/static/css/main.cad71373.chunk.css"
+  },
+  {
+    "revision": "4c623f5adc43c40992ac",
+    "url": "/static/js/2.4f66654e.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.4f66654e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90517af4bef2d322917e",
+    "url": "/static/js/main.a61242c5.chunk.js"
+  },
+  {
+    "revision": "92d2ccaa658bb63a3fc8",
+    "url": "/static/js/runtime-main.51094c6a.js"
+  },
+  {
+    "revision": "3f9d2cf3292607b9ae7ff1361e604162",
+    "url": "/static/media/Green-Tropical-Plant-Wallpaper-Mural-Plain.3f9d2cf3.jpg"
+  }
+]);
